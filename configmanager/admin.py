e502390c11c12941b9b14dea36b708e225ea2f09
Cloud User <ec2-user@ip-172-31-29-163.ap-northeast-2.compute.internal>
2018-05-19 03:24:30 +0000

    configmanager

---
+++ configmanager/admin.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Site, ECS, Configfile
+
+#class ConfigfileInline(admin.TabularInline):
+#    model = Configfile
+#    extra = 0
+
+class ECSAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('server_info',      {'fields': ['name']}),
+        (None,               {'fields': ['instanceid']}),
+        (None,               {'fields': ['IP']}),
+        (None,               {'fields': ['status']}),
+    ]
+
+    list_display = ('name', 'instanceid', 'IP', 'status', 'modified_user', 'modified_time') 
+    list_filter = ['name']
+    search_fields = ['name']
+
+    def save_model(self, request, obj, form, change):
+        obj.modified_user = request.user.username
+        super(ECSAdmin, self).save_model(request, obj, form, change)
+
+
+class SiteAdmin(admin.ModelAdmin):
+
+    def get_ECSlists(self, obj):
+        return ', '.join([e.name for e in obj.ECSlists.all()])
+    get_ECSlists.short_description = 'ECSs'
+ 
+    fieldsets = [
+        ('site_info', {'fields': ['fullname', 'shortname', 'configdirname', 'ECSlists', 'configfiles', 'port', 'testpage', 'status', 'deployattention', 'devcharge']}),
+    ]
+    #inlines = [ConfigfileInline]
+    filter_horizontal = ('ECSlists', 'configfiles')
+
+    list_display = ('fullname', 'get_ECSlists', 'deployattention', 'status', 'modified_time')
+    list_filter = ['port']
+    search_fields = ['fullname']
+
+    def save_model(self, request, obj, form, change):
+        obj.modified_user = request.user.username
+        super(SiteAdmin, self).save_model(request, obj, form, change)
+
+
+class ConfigfileAdmin(admin.ModelAdmin):
+    list_display=('sitecluster', 'filename', 'modified_time')
+
+admin.site.register(ECS, ECSAdmin)
+admin.site.register(Site, SiteAdmin)
+admin.site.register(Configfile, ConfigfileAdmin)
---
+++ configmanager/models.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class ECS(models.Model):
+    ENABLE = 'Y'
+    DISABLE = 'N'
+    status_CHOICES = (
+        (ENABLE, 'enable'),
+        (DISABLE, 'disable'),
+    )
+    name = models.CharField(max_length=30)
+    instanceid = models.CharField(max_length=30)
+    IP = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=1,
+        choices=status_CHOICES,
+        default=ENABLE,
+    )
+    modified_user = models.CharField(max_length=20)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Configfile(models.Model):
+    sitecluster = models.CharField(max_length=30, null=True)
+    filename = models.CharField(max_length=30)
+    content = models.TextField(null=True, blank=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.filename
+
+
+class Site(models.Model):
+    ENABLE = 'Y'
+    DISABLE = 'N'
+    status_CHOICES = (
+        (ENABLE, 'enable'),
+        (DISABLE, 'disable'),
+    )
+    fullname = models.CharField(max_length=30)
+    shortname = models.CharField(max_length=30)
+    configdirname = models.CharField(max_length=50)
+    ECSlists = models.ManyToManyField(ECS)
+    configfiles = models.ManyToManyField(Configfile)
+    port = models.PositiveSmallIntegerField(null=True)
+    testpage = models.CharField(max_length=30, null=True)
+    devcharge = models.CharField(max_length=10, null=True)
+    deployattention = models.TextField(null=True, blank=True)
+    modified_time = models.DateTimeField(auto_now=True)
+    modified_user = models.CharField(max_length=20, default='admin')
+    status = models.CharField(
+        max_length=1,
+        choices=status_CHOICES,
+        default=ENABLE,
+    )
+    
+    def __unicode__(self):
+        return self.fullname
+
+   

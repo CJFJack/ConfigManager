0edee4948ea342b20dffa16aa52dd80cb8e75e45
Cloud User <ec2-user@ip-172-31-29-163.ap-northeast-2.compute.internal>
2018-06-06 11:57:17 +0800

    ecs_add ecs_delete ecs_disbale ecs_enable

--- configmanager/urls.py
+++ configmanager/urls.py
@@ -5,9 +5,14 @@ app_name = 'configmanager'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^ecs$', views.ECSListView.as_view(), name='ecslist'),
-    url(r'^ecs/(?P<pk>[0-9]+)/change$', views.ECSChangeView.as_view(), name='ecschange'),
-    url(r'^ecs/(?P<ecs_id>[0-9]+)/save/$', views.save, name='save'),
+    url(r'^ecs/$', views.ECSListView.as_view(), name='ecslist'),
+    url(r'^ecs/(?P<pk>[0-9]+)/change/$', views.ECSChangeView.as_view(), name='ecschange'),
+    url(r'^ecs/(?P<ecs_id>[0-9]+)/save/$', views.ecs_save, name='ecssave'),
+    url(r'^ecs/(?P<ecs_id>[0-9]+)/enable/$', views.ecs_enable, name='ecsenable'),
+    url(r'^ecs/(?P<ecs_id>[0-9]+)/disable/$', views.ecs_disable, name='ecsdisable'),
+    url(r'^ecs/(?P<ecs_id>[0-9]+)/delete/$', views.ecs_delete, name='ecsdelete'),
+    url(r'^ecs/add/$', views.ECSAddView.as_view(), name='ecsadd'),
+    url(r'^ecs/addmethod/$', views.ecs_add, name='ecsadd_method'),
     url(r'^search-form$', views.search_form),
     url(r'^search$', views.search),
     url(r'^search-post$', views.search_form),
--- configmanager/views.py
+++ configmanager/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.views.decorators import csrf
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 # Create your views here.
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from .models import ECS, Site
 from django.http import HttpResponse
@@ -20,7 +22,7 @@ class ECSListView(generic.ListView):
     template_name = 'configmanager/ecs_list.html'
     context_object_name = 'ECS_list'
     def get_queryset(self):
-        return ECS.objects.order_by('modified_time')
+        return ECS.objects.order_by('name')
 
 
 class ECSChangeView(generic.DetailView):
@@ -28,9 +30,50 @@ class ECSChangeView(generic.DetailView):
     template_name = 'configmanager/ecs_change.html'
 
 
-def save(request, ecs_id):
-    pass
+class ECSAddView(generic.ListView):
+    model = ECS
+    template_name = 'configmanager/ecs_add.html'
 
+
+def ecs_save(request, ecs_id):
+    ecs = get_object_or_404(ECS, pk=ecs_id)
+    ecs.name = request.POST['name']
+    ecs.instanceid = request.POST['instanceid']
+    ecs.IP = request.POST['IP']
+    ecs.status = request.POST['status']
+    ecs.modified_user = request.user.username
+    ecs.save()
+    return HttpResponseRedirect(reverse('configmanager:ecslist'))
+
+
+def ecs_enable(request, ecs_id):
+    ecs = get_object_or_404(ECS, pk=ecs_id)
+    ecs.status = 'Y'
+    ecs.save()
+    return HttpResponseRedirect(reverse('configmanager:ecslist'))
+
+
+def ecs_disable(request, ecs_id):
+    ecs = get_object_or_404(ECS, pk=ecs_id)
+    ecs.status = 'N'
+    ecs.save()
+    return HttpResponseRedirect(reverse('configmanager:ecslist'))
+
+
+def ecs_delete(request, ecs_id):
+    ecs = get_object_or_404(ECS, pk=ecs_id)
+    ecs.delete()
+    return HttpResponseRedirect(reverse('configmanager:ecslist'))
+
+
+def ecs_add(request):
+    name = request.POST['name']                                                                                                       
+    instanceid = request.POST['instanceid']                                                                                           
+    IP = request.POST['IP']                                                                                                           
+    status = request.POST['status']                                                                                                   
+    ecs = ECS(name=name, instanceid=instanceid, IP=IP, status=status, modified_user=request.user.username)
+    ecs.save()                                                                                                                            
+    return HttpResponseRedirect(reverse('configmanager:ecslist'))   
 
 
 
